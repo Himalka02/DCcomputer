aecbd0a375e6396cdd703a2375c0347626d04af7
Himalka02 <154166559+Himalka02@users.noreply.github.com>
2024-08-19 19:41:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    
+}
