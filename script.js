a1474a1967167519168813d43bdbe0eebb3c7db6
Himalka02 <154166559+Himalka02@users.noreply.github.com>
2024-08-20 14:14:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-let searchForm = document.querySelector('.search-form');
+const bar = document.getElementById('bar')
+const close = document.getElementById('close')
+const nav = document.getElementById('navbar')
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
+if(bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
 }
-let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
+if(close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
 }
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let searchForm = document.querySelector('.search-form');
+
+// document.querySelector('#search-btn').onclick = () =>{
+//     searchForm.classList.toggle('active');
+// }
+// let navbar = document.querySelector('.navbar');
+
+// document.querySelector('#menu-btn').onclick = () =>{
+//     navbar.classList.toggle('active');
+// }
+// window.onscroll = () =>{
+//     navbar.classList.remove('active');
+//     searchForm.classList.remove('active');
     
-}
+// }
